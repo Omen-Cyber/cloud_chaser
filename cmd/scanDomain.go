69258ebbaf65676816adac4ea14f61808d23ff44
ak <fkrepelka@gmail.com>
2024-08-26 01:47:47 +0300

    cleaned up project

--- cmd/scanDomain.go
+++ cmd/scanDomain.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 ak ak@omencyber.io
 */
 package cmd
 
---
+++ runners/dnsScanner.go
@@ -0,0 +1,14 @@
+/*
+Copyright © 2024 ak ak@omencyber.io
+*/
+package runners
+import (
+	"github.com/Omen-Cyber/cloud_chaser/cmd"
+)
+
+func dnsScan(
+)
+
+{
+
+}
